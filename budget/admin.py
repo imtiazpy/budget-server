4f1a9cca6bd7456b3d31ba0a602c880f819e1a6e
imtiazpy <ahmed.imti1994@gmail.com>
2022-06-11 03:26:36 +0600

    added some property method to the models and modified admin view

--- budget/admin.py
+++ budget/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from budget.models import Budget, Category, CategoryItem
 
 
-admin.site.register(Budget)
-admin.site.register(Category)
-admin.site.register(CategoryItem)
+class CustomBudgetAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'income_monthly_total', 'expense_monthly_total', 'surplus_monthly')
+
+class CustomCategoryAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'total_monthly', 'budget')
+
+class CustomCategoryItemAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'monthly', 'category', 'get_budget')
+
+admin.site.register(Budget, CustomBudgetAdmin)
+admin.site.register(Category, CustomCategoryAdmin)
+admin.site.register(CategoryItem, CustomCategoryItemAdmin)
