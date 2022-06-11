cdfcfdc30a40703c9d5c8c37488a02f9bdd4c2b0
imtiazpy <ahmed.imti1994@gmail.com>
2022-06-11 11:46:07 +0600

    added filter in budget admin

--- budget/admin.py
+++ budget/admin.py
@@ -5,12 +5,15 @@ from budget.models import Budget, Category, CategoryItem
 
 class CustomBudgetAdmin(admin.ModelAdmin):
     list_display = ('get_name', 'income_monthly_total', 'expense_monthly_total', 'surplus_monthly')
+    list_filter = ('created_by', )
 
 class CustomCategoryAdmin(admin.ModelAdmin):
     list_display = ('get_name', 'total_monthly', 'budget')
+    list_filter = ('budget', 'c_type', )
 
 class CustomCategoryItemAdmin(admin.ModelAdmin):
     list_display = ('get_name', 'monthly', 'category', 'get_budget')
+    list_filter = ('category', )
 
 admin.site.register(Budget, CustomBudgetAdmin)
 admin.site.register(Category, CustomCategoryAdmin)
